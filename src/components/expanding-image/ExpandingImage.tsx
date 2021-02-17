2510bda0da95257cb763832fa19eb90c2f7329a2
kainDoneson@gmail.com <kaindoneson@gmail.com>
2021-02-17 18:26:29 -0500

    based on feedback

--- src/components/expanding-image/ExpandingImage.tsx
+++ src/components/expanding-image/ExpandingImage.tsx
@@ -7,7 +7,12 @@ interface Props {
 const ExpandingImage: React.FC<Props> = function ({ imageUrl }: Props) {
     return (
         <div className="expanding-image-container">
-            <div className="expanding-image" />
+            <div
+                className="expanding-image"
+                style={{
+                    backgroundImage: `linear-gradient(to right, #116466, rgba(4, 89, 110, 0.363)), url(${imageUrl})`,
+                }}
+            />
         </div>
     );
 };
